35cdb27afd9fc16acfcd737d90ee14ad84ab04b7
Alexandre Oliveira <alexmdo@gmail.com>
2023-04-20 20:09:23 -0300

    Error handling

---
+++ src/main/java/med/voll/api/infra/ErrorHandler.java
@@ -0,0 +1,35 @@
+package med.voll.api.infra;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Void> handleError404() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ErrorValidationData>> handleError400(MethodArgumentNotValidException e) {
+        var errors = e.getFieldErrors();
+        return ResponseEntity.badRequest().body(errors.stream().map(ErrorValidationData::new).toList());
+    }
+
+    private record ErrorValidationData(String field, String message) {
+
+        public ErrorValidationData(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+
+    }
+    
+}
