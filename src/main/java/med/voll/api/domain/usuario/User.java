cffed55f1fef157c198f8be82733ca33950a576f
Alexandre Oliveira <alexmdo@gmail.com>
2023-04-21 21:43:59 -0300

    User entity creation

---
+++ src/main/java/med/voll/api/domain/usuario/User.java
@@ -0,0 +1,26 @@
+package med.voll.api.domain.usuario;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String password;
+    
+}
