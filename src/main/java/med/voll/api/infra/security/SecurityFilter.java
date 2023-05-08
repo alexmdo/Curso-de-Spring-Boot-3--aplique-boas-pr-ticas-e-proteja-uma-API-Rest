04c639282427e3f18a58a51c80f1f8cd8139ce85
Alexandre Oliveira <alexmdo@gmail.com>
2023-05-07 22:51:08 -0300

    Authenticating the user

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -2,6 +2,8 @@ package med.voll.api.infra.security;
 
 import java.io.IOException;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -9,21 +11,33 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import med.voll.api.domain.usuario.UserRepository;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
     private final TokenService tokenService;
+    private final UserRepository userRepository;
 
-    public SecurityFilter(TokenService tokenService) {
+    public SecurityFilter(TokenService tokenService, UserRepository userRepository) {
         this.tokenService = tokenService;
+        this.userRepository = userRepository;
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         var tokenJWT = getBearerToken(request);
-        var subject = tokenService.getSubject(tokenJWT);
+        if (tokenJWT != null) {
+            var subject = tokenService.getSubject(tokenJWT);
+            var user = userRepository.findByLogin(subject);
+
+            if (user.isPresent()) {
+                var userDetails = user.get();
+                var authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
 
         filterChain.doFilter(request, response);
     }
@@ -32,9 +46,9 @@ public class SecurityFilter extends OncePerRequestFilter {
         var authHeader = request.getHeader("Authorization");
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
             return authHeader.replace("Bearer ", "");
-        } else {
-            throw new SecurityException("Invalid token");
         }
+
+        return null;
     }
 
 }
